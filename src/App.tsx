ce117c9c5025f69d5fe975b9630a7a47b7d0a90b
Binij Shrestha <shresthabinij@gmail.com>
2023-04-21 15:00:12 +0545

    fix pi error

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState } from "react";
+import { AuthResult, PaymentDTO, User } from "./types";
+import Header from "./components/Headers";
+import SignIn from "./components/SignIn";
+
+import "./App.css";
+import ItemCard from "./components/ItemCard";
+
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+
+const axiosClient = axios.create({
+  baseURL: `${backendURL}`,
+  timeout: 20000,
+  withCredentials: true,
+});
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState<User | null>(null);
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const signIn = async () => {
+    const scopes = ["username", "payments"];
+    const authResult: AuthResult = await window.Pi.authenticate(
+      scopes,
+      onIncompletePaymentFound
+    );
+    signInUser(authResult);
+    setUser(authResult.user);
+  };
+
+  const signOut = () => {
+    setUser(null);
+    signOutUser();
+  };
+  const signInUser = (authResult: AuthResult) => {
+    axiosClient.post("/user/signin", { authResult });
+    return setShowModal(false);
+  };
+
+  const signOutUser = () => {
+    return axiosClient.get("/user/signout");
+  };
+
+  const orderItem = async (memo: string, amount: number, paymentMetadata: {}) => {
+    if(user === null) {
+      return setShowModal(true);
+    }
+    const paymentData = { amount, memo, metadata: paymentMetadata };
+    const callbacks = {
+      onReadyForServerApproval,
+      onReadyForServerCompletion,
+      onCancel,
+      onError
+    };
+    const payment = await window.Pi.createPayment(paymentData, callbacks);
+    console.log(payment);
+  }
+
+  const onModalClose = () => {
+    setShowModal(false);
+  };
+
+  const onIncompletePaymentFound = (payment: PaymentDTO) => {
+    console.log("onIncompletePaymentFound", payment);
+    return axiosClient.post("/payments/incomplete", { payment });
+  };
+
+  const onReadyForServerApproval = (paymentId: string) => {
+    console.log("onReadyForServerApproval", paymentId);
+    axiosClient.post("/payments/approve", { paymentId }, config);
+  };
+
+  const onReadyForServerCompletion = (paymentId: string, txid: string) => {
+    console.log("onReadyForServerCompletion", paymentId, txid);
+    axiosClient.post("/payments/complete", { paymentId, txid }, config);
+  };
+
+  const onCancel = (paymentId: string) => {
+    console.log("onCancel", paymentId);
+    return axiosClient.post("/payments/cancelled_payment", { paymentId });
+  };
+
+  const onError = (error: Error, payment?: PaymentDTO) => {
+    console.log("onError", error);
+    if (payment) {
+      console.log(payment);
+      // handle the error accordingly
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header user={user} onSignIn={signIn} onSignOut={signOut} />
+
+      <h1>Welcome to NFT Market</h1>
+      <ItemCard
+        name="Anup Jain "
+        description="Anup jain song. Anup jain song. Anup jain song. Anup jain song."
+        price={1}
+        pictureURL="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Apple_pie.jpg/1280px-Apple_pie.jpg"
+        pictureCaption="Picture by Dan Parsons"
+        onClickBuy={()=>orderItem("Order Anup Jain", 1, { productId: 'anup_jain_101' })}
+      
+      />
+      {showModal && <SignIn onSignIn={signIn} onModalClose={onModalClose} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            backendURL: env.REACT_APP_BACKEND_URL,
+            sandbox: env.REACT_APP_SANDBOX_SDK,
+          },
+        },
+      }),
+    ],
+  };
+});
